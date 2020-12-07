382a16c808e25386acaf70dc992e119e0751bd92
Stuart Burrell <stuartburrell1994@gmail.com>
2020-12-07 16:01:22 +0000

    Improve Day 7 solution

--- day7.py
+++ day7.py
@@ -11,7 +11,7 @@ for i in range(len(inputs)):
                                             ' ','')
     inputs[i][1] = inputs[i][1][:-2].split(',')
  
-# Arrange data into dictionary
+# Arrange data as a dictionary
 
 rules = dict()
 for rule in inputs:
