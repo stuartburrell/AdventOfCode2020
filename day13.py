d9177bd65faa07b5c160de357921b3503ce4eaeb
Stuart Burrell <stuartburrell1994@gmail.com>
2020-12-13 17:23:54 +0000

    Add CRT code

--- day13.py
+++ day13.py
@@ -13,8 +13,6 @@ print('Part 1: ', ids[ind_best_bus]*wait_times[ind_best_bus] )
 
 # Part 2
 
-offsets = {x : inputs[1].split(',').index(str(x)) for x in ids}
-
 '''
 Apply Chinese Remainder Theorem: we seek t such that
  
@@ -26,7 +24,31 @@ t mod 733 = 733 - 23
 .
 t mod bus_ID = bus_ID - offset[bus_ID]
 
-Using a standard CRT solver: t = 775230782877242.
+CRT code: https://rosettacode.org/wiki/Chinese_remainder_theorem#Python_3.6
 '''
+     
+from functools import reduce
 
-print('Part 2: ', 775230782877242)
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a%b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: x1 += b0
+    return x1
+
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda a, b: a*b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+
+offsets = {x : inputs[1].split(',').index(str(x)) for x in ids}
+remainders = [x - offsets[x] for x in ids]
+
+print('Part 2: ', chinese_remainder(ids, remainders))
